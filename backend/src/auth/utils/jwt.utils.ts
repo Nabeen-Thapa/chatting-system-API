e4011cb3c48f8bb630cd270eb1c17f89ff75b38e
Nabeen-Thapa <nabeen2058@gmail.com>
2026-02-13 23:13:10 +0545

    add redis setup

---
+++ backend/src/auth/utils/jwt.utils.ts
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+
+const ACCESS_SECRET = process.env.JWT_ACCESS_SECRET || 'access_secret';
+const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || 'refresh_secret';
+
+export function signAccessToken(payload:any){
+    return jwt.sign(payload, ACCESS_SECRET, {
+    expiresIn: '15m',
+    })
+}
+
+export function signRefreshToken(payload: any) {
+  return jwt.sign(payload, REFRESH_SECRET, {
+    expiresIn: '7d',
+  });
+}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { ConflictException, forwardRef, Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { EntityManager } from '@mikro-orm/postgresql';
@@ -6,25 +6,34 @@ import { User } from './entities/user.entity';
 import { RegisterDto } from '../auth/dto/create-user.dto';
 import { AuthService } from '../auth/auth.service';
 import { LoginDto } from '../auth/dto/login-user.dto';
+import { RedisService } from '../redis/redis.service';
 
 @Injectable()
 export class UserService {
- constructor(
+  constructor(
     private readonly em: EntityManager,
-    @Inject(forwardRef(() => AuthService)) 
+    @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
-  ) {}
-    
+    private readonly redisService: RedisService,
+  ) { }
 
- async create(data: RegisterDto) {
+
+  async create(data: RegisterDto) {
+    const cacheKey = `user:email:${data.email}`;
+    const cachedUser = await this.redisService.get(cacheKey);
+    if (cachedUser) throw new ConflictException("user already eixist with this email")
+
     const user = this.em.create(User, data);
     await this.em.persistAndFlush(user);
+
+    await this.redisService.set(cacheKey, user, 3600);
     return user;
-    return 'This action adds a new user';
   }
 
   async login(data: LoginDto) {
 
+    const cachedUser = await this.redisService.get(`user:email:${data.email}`);
+   
     return 'This action login user';
   }
 
